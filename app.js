e06236b538abe1548df5e52d88358b5b8af4836b
Brandon Dionisio <brandon.dionisio.26@gmail.com>
2017-10-04 11:24:15 -0700

    Add constants directory and export user command constants to app.js

--- app.js
+++ app.js
@@ -1,4 +1,24 @@
 const fs = require('fs');
 const argv = require('yargs').argv;
 
+const commandConstants = require('./constants');
+const { add, remove, play, list } = commandConstants;
+
 const userCommand = argv._[0];
+
+switch (userCommand) {
+  case add:
+    console.log('add song');
+    break;
+  case remove:
+    console.log('remove song');
+    break;
+  case play:
+    console.log('play song');
+    break;
+  case list:
+    console.log('list all songs');
+    break;
+  default:
+    console.log('Sorry, command not recognized');
+}
---
+++ constants/index.js
@@ -0,0 +1,11 @@
+const add = 'add';
+const remove = 'remove';
+const play = 'play';
+const list = 'list';
+
+module.exports = {
+  add,
+  remove,
+  play,
+  list
+};
